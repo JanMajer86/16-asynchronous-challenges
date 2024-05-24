ccf680e45b14e44fb422b7e0336ca142cb1610a1
JanMajer86 <majer.jan.1986@gmail.com>
2024-05-24 20:32:42 +0200

    done

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 "use strict";
 
+const countriesContainer = document.querySelector(".countries");
+
 console.log("where am i");
+
+const renderCountry = function (data) {
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.subregion}</h4>
+            <p class="country__row"><span>👫</span>${(
+                data.population / 1000000
+            ).toFixed(1)} mil. people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+                data.currencies[0].name
+            }</p>
+        </div>
+    </article>
+    `;
+
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+};
 
 const whereAmI = function (lat, long) {
     fetch(
@@ -14,15 +37,32 @@ const whereAmI = function (lat, long) {
             console.log(
                 `You are in ${data.address.city}, ${data.address.country}`
             );
+            getCountryData(`${data.address.country}`);
         })
         .catch((err) => {
             console.log(`${err} 🤬🤬🤬`);
         });
 };
 
-whereAmI(52.508, 13.381);
+const getCountryData = function (country) {
+    fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+        .then((response) => {
+            if (!response.ok)
+                throw new Error(`Country not found (${response.status})`);
+
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data[0]);
+            renderCountry(data[0]);
+        })
+        .catch((err) => console.error(`${err} 🤬🤬🤬`))
+        .finally(() => (countriesContainer.style.opacity = 1));
+};
+
+// whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
-// whereAmI(-33.933, 18.474);
+whereAmI(-33.933, 18.474);
 
 /*
 token
